8c12dd26dda0678c928493479b893fd164757773
minibear <lei.zhang.bupt08@gmail.com>
2016-08-30 11:34:04 +0800

    MOD:

---
+++ public/dashboard/javascripts/package.js
@@ -0,0 +1,16 @@
+(function ($) {
+  'use strict';
+
+  $(function () {
+    $('#wizard').smartWizard();
+    $('.buttonNext').addClass('btn btn-success');
+    $('.buttonPrevious').addClass('btn btn-primary');
+    $('.buttonFinish').addClass('btn btn-default');
+    $('#package-tags').tagsInput({
+      width: 'auto'
+    });
+    // $('#package-thumbnails').dropzone({
+    //   url: '/package/thumbnails/upload'
+    // });
+  });
+}(jQuery));
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,14 +4,14 @@ var Package = require('../models/package');
 
 
 router.get('/', function(req, res, next) {
-
+  res.render('dashboard/layout');
 });
 
 /**
  * 线路页面GET请求
  */
 router.get('/package', function(req, res, next) {
-
+  res.render('dashboard/package');
 });
 
 /**
