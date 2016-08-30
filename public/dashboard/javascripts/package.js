eaa83b041e6ee7e803b444625487354575d515a0
Jason Zhang <jzhang@skystarstudio.com>
2016-08-29 22:28:49 -0700

    MOD: form wizard style update

--- public/dashboard/javascripts/package.js
+++ public/dashboard/javascripts/package.js
@@ -2,13 +2,19 @@
   'use strict';
 
   $(function () {
-    $('#wizard').smartWizard();
-    $('.buttonNext').addClass('btn btn-success');
-    $('.buttonPrevious').addClass('btn btn-primary');
-    $('.buttonFinish').addClass('btn btn-default');
+
     $('#package-tags').tagsInput({
       width: 'auto'
     });
+
+    var formWizard = $('#wizard');
+    formWizard.smartWizard();
+    formWizard.find('.stepContainer').css('height', 'auto');
+
+    $('.buttonNext').addClass('btn btn-success');
+    $('.buttonPrevious').addClass('btn btn-primary');
+    $('.buttonFinish').addClass('btn btn-default');
+
     // $('#package-thumbnails').dropzone({
     //   url: '/package/thumbnails/upload'
     // });
